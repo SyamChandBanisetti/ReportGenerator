0d9a6027d92698ee33677fd7d0d9c515dcf24c90
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-04-19 16:41:57 +0530

    Update leetcode_fetcher.py

--- utils/leetcode_fetcher.py
+++ utils/leetcode_fetcher.py
@@ -1,26 +1,21 @@
 import requests
 
 def fetch_leetcode_stats(username):
-    url = "https://leetcode.com/graphql"
-    query = """
-    query getUserProfile($username: String!) {
-      matchedUser(username: $username) {
-        submitStats: submitStatsGlobal {
-          acSubmissionNum {
-            difficulty
-            count
-          }
-        }
-      }
-    }
     """
-    variables = {"username": username}
-    response = requests.post(url, json={"query": query, "variables": variables})
+    Fetch the number of submissions made by the student on LeetCode.
+    """
+    url = f"https://leetcode.com/{username}/"
     
-    if response.status_code == 200:
-        data = response.json()
-        submissions = data["data"]["matchedUser"]["submitStats"]["acSubmissionNum"]
-        total_solved = sum(item["count"] for item in submissions if item["difficulty"] != "All")
-        return total_solved
-    else:
-        return 0
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        # Extracting the number of submissions from the HTML page
+        submissions_section = response.text.split('submissions={')[1].split('}')[0]
+        submissions_data = eval(f"{{{submissions_section}}}")  # Convert to dictionary
+        total_submissions = submissions_data.get('total', 0)
+        
+        return total_submissions
+    except Exception as e:
+        print(f"Error fetching LeetCode data: {e}")
+        return 0  # Return 0 if there is an issue

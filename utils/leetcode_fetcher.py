9475f2d150cb6c5381d86938a57d9ddf3d46d1d8
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-04-19 17:10:35 +0530

    Update leetcode_fetcher.py

--- utils/leetcode_fetcher.py
+++ utils/leetcode_fetcher.py
@@ -1,18 +1,18 @@
-# leetcode_fetcher.py
+# utils/leetcode_fetcher.py
+
 import requests
 
-def fetch_leetcode_stats(username):
-    """
-    Fetches the number of Leetcode submissions made by the given user.
-    Returns the number of submissions.
-    """
-    url = f"https://leetcode.com/{username}/"
+def fetch_leetcode_data(username):
+    url = f"https://leetcode-stats-api.herokuapp.com/{username}"
     response = requests.get(url)
-    
     if response.status_code == 200:
-        # Scraping the number of problems solved from the user's profile page
-        solved = response.text.split('class="total-solved-problems"')[1]
-        solved = solved.split('<span>')[1].split('</span>')[0]
-        return int(solved)
+        data = response.json()
+        return {
+            "totalSolved": data.get("totalSolved", 0),
+            "easySolved": data.get("easySolved", 0),
+            "mediumSolved": data.get("mediumSolved", 0),
+            "hardSolved": data.get("hardSolved", 0),
+            "ranking": data.get("ranking", "N/A")
+        }
     else:
-        return 0
+        raise Exception("Failed to fetch data from LeetCode API")
